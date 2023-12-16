ebc04e408f96dfb356229ad527472c3197d8b2ae
CORP\palnredd <palnati-ram.reddy@capgemini.com>
2023-12-16 10:16:11 +0530

    test deploy

--- src/CartContainer.jsx
+++ src/CartContainer.jsx
@@ -9,7 +9,7 @@ const CartContainer = () => {
     return (
       <section className="cart">
         <header>
-          <h2>your bag</h2>
+          <h2>Ram your bag</h2>
           <h4 className="empty-cart">is currently empty</h4>
         </header>
       </section>
